9ce4f8ac4b898c5bf2bb9f0d79e031f3e976d7be
migSig <info@migsig.com>
2015-12-15 13:40:42 -0500

    removed migrate test files

--- db/migrate/20151215155821_add_description_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration
-  def change
-      add_column :articles, :description, :text
-      add_column :articles,:created_at, :datetime
-      add_column :articles,:upadte_at, :datetime
-  end
-end
