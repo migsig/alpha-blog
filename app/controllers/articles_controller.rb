c583a7e97f2499e617413c3adaa6be3936afa3d4
migSig <info@migsig.com>
2015-12-19 15:58:15 -0500

    Article show edit controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,18 @@ class ArticlesController < ApplicationController
     def new
       @article = Article.new   
     end
-    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was succesfully updated"
+          redirect_to article_path(@article)
+      else
+            render 'edit'
+      end
+    end
     def create
       @article = Article.new(article_params)
       if @article.save
