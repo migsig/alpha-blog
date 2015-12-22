831d6ca184cb5fc246a7391bb33ab42d0a25d0b4
migSig <info@migsig.com>
2015-12-22 17:35:23 -0500

    Created Destroy action and updated links to all CRUD Actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,12 +30,16 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
-    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "The selected article was deleted."
+        redirect_to articles_path
+    end
     
     
     private
       def article_params
         params.require(:article).permit(:title,:description)
       end
-    
 end
