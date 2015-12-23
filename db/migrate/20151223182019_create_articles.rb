9545b69d12da9532d79e5488370d45f77377b900
migSig <info@migsig.com>
2015-12-23 14:09:01 -0500

    database table for production

--- db/migrate/20151223182019_create_articles.rb
+++ db/migrate/20151223182019_create_articles.rb
@@ -2,6 +2,8 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
+      t.text :description
+      t.timestamps
     end
   end
 end
