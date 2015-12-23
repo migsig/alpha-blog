b190fb743e7dddbb2687bc967ef30df93179d529
migSig <info@migsig.com>
2015-12-22 19:27:36 -0500

    extract redundancy and prepare for porduction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,44 @@
 class ArticlesController < ApplicationController
-    def index
-        @articles = Article.all
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
+  def index
+    @articles = Article.all
+  end
+  def new
+    @article = Article.new   
+  end
+  def edit    
+  end
+  def update
+    if @article.update(article_params)
+        flash[:notice] = "Article was succesfully updated"
+        redirect_to article_path(@article)
+    else
+        render 'edit'
     end
-    def new
-      @article = Article.new   
+  end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+        flash[:notice] = "Article was succesfully created"
+        redirect_to article_path(@article)
+    else
+        render 'new'
     end
-    def edit
-        @article = Article.find(params[:id])
+  end
+  def show
+  end
+  def destroy
+    @article.destroy
+    flash[:notice] = "The selected article was deleted."
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title,:description)
     end
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(article_params)
-            flash[:notice] = "Article was succesfully updated"
-          redirect_to article_path(@article)
-      else
-            render 'edit'
-      end
-    end
-    def create
-      @article = Article.new(article_params)
-      if @article.save
-          flash[:notice] = "Article was succesfully created"
-          redirect_to article_path(@article)
-      else
-          render 'new'
-      end
-    end
-   def show
+    def set_article
       @article = Article.find(params[:id])
     end
-
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-        flash[:notice] = "The selected article was deleted."
-        redirect_to articles_path
-    end
-    
-    
-    private
-      def article_params
-        params.require(:article).permit(:title,:description)
-      end
 end
