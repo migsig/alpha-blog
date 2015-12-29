d9a74550f015a7411602d4212f71eeb3daaaf863
migSig <info@migsig.com>
2015-12-29 17:01:44 -0500

    Bootstrap 3.3.6

--- db/migrate/20151223203715_create_articles.rb
+++
@@ -1,7 +0,0 @@
-class CreateArticles < ActiveRecord::Migration
-  def change
-    create_table :articles do |t|
-      t.string :title
-    end
-  end
-end
--- db/migrate/20151223203719_add_description_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration
-  def change
-    add_column :articles, :description, :text
-    add_column :articles, :created_at, :datetime
-    add_column :articles, :updated_at, :datetime
-  end
-end
