7c48027aac8a5724dc6179df39f0810867c88d0c
migSig <info@migsig.com>
2016-02-25 17:46:03 -0500

    Create User Model Validations

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    validates :username, presence:true, uniqueness:{case_sensitive: false}, length: {minimum: 3, maximum: 25}
+    VALID_EMAIL_REGEX:/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence:true, uniqueness:{case_sensitive: false}, length: {maximum: 105},
+    format: {with: VALID_EMAIL_REGEX }
 end
