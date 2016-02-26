29d2f1b4e8f7dc01447689d7d6088437f73d1cea
migSig <info@migsig.com>
2016-02-26 16:48:41 -0500

    Edited Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
-  validate :user_id, presence:true
+  validates :user_id, presence:true
 end
